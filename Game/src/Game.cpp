de45f2dae1bf4045211189e6e6658ea3b606440e
Hemanth3303 <109304485+Hemanth3303@users.noreply.github.com>
2024-02-06 01:54:36 +0530

    added hart as submodule and set up a simple blank app

---
+++ Game/src/Game.cpp
@@ -0,0 +1,19 @@
+#include "Hart.hpp"
+
+class Game : public Hart::Application {
+public:
+	Game() 
+		: Application(640, 480, "Hart-Engine: Game-Template", true) {
+		HART_CLIENT_LOG("Init Game");
+		setBackgroundColor(Hart::Black);
+		setExitKey(Hart::KeyCode::Escape);
+		setMaxFPS(144);
+	}
+private:
+
+};
+
+
+std::unique_ptr<Hart::Application> Hart::CreateApplication() {
+	return std::make_unique<Game>();
+}
